fb865727caba394688d91d3cffc5727a2071989c
nunera <67439647+nunera@users.noreply.github.com>
2023-11-29 15:35:37 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 from colorama import Fore
+from time import sleep
 
 config = {
     "replaceSpawnsAs" : "P"
@@ -16,6 +17,7 @@ _____________________
 '''
 gameInfo = {
     'char' : {
+        'location' : [],
         'name' : '',
         'health' : 100,
         'hunger' : 100,
@@ -64,27 +66,57 @@ def find_all_gen(str, q):
     while i != -1:
         yield i
         i = str.find(q, i+1)
+        
+class gameHandler():
+    
+    def __init__(self, map, config, definitions, gameInfo):
+        self.map = map
+        self.config = config
+        self.definitions = definitions
+        self.gameInfo = gameInfo
+        
+    def mapSetup(self):
+        spawns = []
+        rp = self.config["replaceSpawnsAs"]
+        m = self.map.split("\n")
+        m.pop(0)
+        m.pop(-1)
+        for i,v in enumerate(m):
+            for r in findall(v,"S"):
+                spawns.append([i,r])
+        spawn = random.choice(spawns)
+        self.gameInfo['char']['location'] = spawn
+        spawns.remove(spawn)
+        sline = m[spawn[0]]
+        sline = sline[:spawn[1]] + "C" + sline[spawn[1]+1:]
+        m[spawn[0]] = sline
+        for i,v in enumerate(m):
+            m[i] = v.replace("S",rp)
+            for k,z in enumerate(self.definitions):
+                if z in v:
+                    for g in findall(v,z):
+                        self.gameInfo['worldEntities'][(i,g)] = self.definitions[z]
+        return m
+    
+    def charSetup(self):
+        input('''
+______ _             _           _   _____            _   _ 
+| ___ \\ |           | |         (_) /  __ \\          | | (_)
+| |_/ / | __ _ _   _| |__   ___  _  | /  \\/ __ _ _ __| |_ _ 
+|  __/| |/ _` | | | | '_ \\ / _ \\| | | |    / _` | '__| __| |
+| |   | | (_| | |_| | |_) | (_) | | | \\__/\\ (_| | |  | |_| |
+\\_|   |_|\\__,_|\\__, |_.__/ \\___/|_|  \\____/\\__,_|_|   \\__|_|
+                __/ |                                       
+                |___/                                 
+            
+                    TEXT ADVENTURE    
+        
+                PRESS [ENTER] TO START.   
+                            ''')
+        print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+        self.gameInfo['char']['name'] = input("Please enter your name.\n\n> ")
+            
 
-def mapSetup(m):
-    spawns = []
-    rp = config["replaceSpawnsAs"]
-    m = m.split("\n")
-    m.pop(0)
-    m.pop(-1)
-    for i,v in enumerate(m):
-        for r in findall(v,"S"):
-            spawns.append([i,r])
-    spawn = random.choice(spawns)
-    spawns.remove(spawn)
-    sline = m[spawn[0]]
-    sline = sline[:spawn[1]] + "C" + sline[spawn[1]+1:]
-    m[spawn[0]] = sline
-    for i,v in enumerate(m):
-        m[i] = v.replace("S",rp)
-        for k,z in enumerate(definitions):
-            if z in v:
-                for g in findall(v,z):
-                    gameInfo['worldEntities'][(i,g)] = definitions[z]
-    return m
-
-map = mapSetup(map)
+game = gameHandler(map, config, definitions, gameInfo)
+game.mapSetup()
+game.charSetup()
