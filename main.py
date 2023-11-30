a0378a0dd5a2dfaba144ad828ac343e9bcca8c9e
nunera <67439647+nunera@users.noreply.github.com>
2023-11-30 08:51:16 -0600

    Update main.py

--- main.py
+++ main.py
@@ -53,7 +53,17 @@ definitions = {
                 },
             },
         },
+        'quotes' : [
+            'you see a wolf with 10 health.'
+        ]
     },
+    'P' : {
+        'name' : 'Path',
+        'quotes' : [
+            'you see a path. You cannot see beyond it.',
+            'is a road. The fog conceals what is past it.'
+        ]
+    }
 }
 
 # this is so stupid that I have to do all this, it should be built in
@@ -66,7 +76,12 @@ def find_all_gen(str, q):
     while i != -1:
         yield i
         i = str.find(q, i+1)
-        
+
+def clean(s):
+    s = s.strip()
+    s = s.lower()
+    return s
+    
 class gameHandler():
     
     def __init__(self, map, config, definitions, gameInfo):
@@ -88,15 +103,16 @@ class gameHandler():
         self.gameInfo['char']['location'] = spawn
         spawns.remove(spawn)
         sline = m[spawn[0]]
-        sline = sline[:spawn[1]] + "C" + sline[spawn[1]+1:]
+        sline = sline[:spawn[1]] + "P" + sline[spawn[1]+1:]
         m[spawn[0]] = sline
         for i,v in enumerate(m):
             m[i] = v.replace("S",rp)
+            v = m[i]
             for k,z in enumerate(self.definitions):
                 if z in v:
                     for g in findall(v,z):
                         self.gameInfo['worldEntities'][(i,g)] = self.definitions[z]
-        return m
+        self.map = m
     
     def charSetup(self):
         input('''
@@ -115,8 +131,54 @@ ______ _             _           _   _____            _   _
                             ''')
         print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
         self.gameInfo['char']['name'] = input("Please enter your name.\n\n> ")
+        print(f"\nYour name is {self.gameInfo['char']['name']}. You look around.\n")
+    def render(self):
+        print(self.gameInfo['worldEntities'])
+        N = False
+        E = False
+        S = False
+        W = False
+        print("\n\n")
+        pos = self.gameInfo['char']['location']
+        i = 0
+        if (pos[0]-1,pos[1]) in self.gameInfo['worldEntities']:
+            N = True
+            north = "To the north "+random.choice(self.gameInfo['worldEntities'][(pos[0]-1,pos[1])]['quotes'])
+            print(f"[N] {north}")
+        if (pos[0]+1,pos[1]) in self.gameInfo['worldEntities']:
+            S = True
+            south = "To the south "+random.choice(self.gameInfo['worldEntities'][(pos[0]+1,pos[1])]['quotes'])
+            print(f"[S] {south}")
+        if (pos[0],pos[1]+1) in self.gameInfo['worldEntities']:
+            E = True
+            east = "To the east "+random.choice(self.gameInfo['worldEntities'][(pos[0],pos[1]+1)]['quotes'])
+            print(f"[E] {east}")
+        if (pos[0],pos[1]-1) in self.gameInfo['worldEntities']:
+            W = True
+            west = "To the west "+random.choice(self.gameInfo['worldEntities'][(pos[0],pos[1]-1)]['quotes'])
+            print(f"[W] {west}")
+        while True:
+            go = clean(input("\nPlease enter where you wish to go.\n> "))
+            if go == "n" and N:
+                self.gameInfo['char']['location'] = [pos[0]-1,pos[1]]
+                break
+            elif go == "e" and E:
+                self.gameInfo['char']['location'] = [pos[0],pos[1]+1]
+                break
+            elif go == "s" and S:
+                self.gameInfo['char']['location'] = [pos[0]+1,pos[1]]
+                break
+            elif go == "w" and W:
+                self.gameInfo['char']['location'] = [pos[0],pos[1]-1]
+                break
+            else:
+                print("\nThat is not an option. Try again.")
+            
+        
             
 
 game = gameHandler(map, config, definitions, gameInfo)
 game.mapSetup()
 game.charSetup()
+while True:
+    game.render()
